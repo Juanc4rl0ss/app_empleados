89730b93d771eed32747a5163c3c15f8f61bfe44
Juanc4rl0ss <juan_c1985@hotmail.es>
2024-10-08 16:48:11 +0200

    proyecto que crea empleados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Listado de Empleados';
+
+  empleados:Empleado[] = [
+    new Empleado ("Juan","Ayala","Presidente",7500),
+    new Empleado ("Ana","Ayala","Directora",7500),
+    new Empleado ("María","Ayala","Jefa sección",7500),
+    new Empleado ("Laura","Ayala","Administrativo",7500),
+  ];
+
+  cuadroNombre:string = "";
+  cuadroApellido:string = "";
+  cuadroCargo:string = "";
+  cuadroSalario:number = 0;
+
+  agregarEmpleado(){
+    let miEmpleado = new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
+    this.empleados.push(miEmpleado);
+  }
 }
